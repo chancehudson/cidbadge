4fe6e9f413f2bbfb6bb25a5008427aa736b7957f
Chance Hudson <jchancehud@gmail.com>
2019-01-15 05:31:10 -0600

    Improve options argument type

--- cidbadge.d.ts
+++ cidbadge.d.ts
@@ -1,7 +1,7 @@
 declare module 'cidbadge' {
-  function cidbadge(cid: string, options: {
-    targetLength: number,
-    href: string
+  function cidbadge(cid: string, options?: {
+    targetLength?: number,
+    href?: string
   }): string;
   export = cidbadge;
 }
